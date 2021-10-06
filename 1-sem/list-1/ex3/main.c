9bae5b7e5256eaab13ee0ef81f9a0fce14b11744
Jakub Drzewiecki <kuba.drzewiecki@icloud.com>
2021-10-06 14:27:21 +0200

    Finished exercise 3

---
+++ 1-sem/list-1/ex3/main.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int drukujProstokat(int n){
+    for(int i=0; i<n; i++){
+        for(int j=0; j<2*n; j++){
+            printf("*");
+        }
+        printf("\n");
+    }
+}
+
+int main()
+{
+    int n=0;
+
+    printf("Zadanie 3\n");
+
+    printf("Wprowadz liczbe od 1 do 20: ");
+    scanf("%d", &n);
+
+    if(n < 1 || n > 20){
+        printf("Wprowadzono niepoprawna liczbe");
+    }
+    else
+        drukujProstokat(n);
+    return 0;
+}
