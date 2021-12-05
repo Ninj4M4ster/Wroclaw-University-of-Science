f532832759079831cbb7b5fc0cd581dff638724c
Ninj4M4ster <kuba.drzewiecki@icloud.com>
2021-12-06 00:43:13 +0100

    One fix to code

--- 1-sem/list-4/ex-1/match.c
+++ 1-sem/list-4/ex-1/match.c
@@ -2,10 +2,8 @@
 
 #include "funs.h"
 #include <cstring>
-#include <assert.h>
 
 bool match(char* wzorzec, char* lancuch){
-    assert(strlen(wzorzec) < strlen(lancuch));
     int i=0, j=0;
     while(i < strlen(wzorzec) && j < strlen(lancuch)) {
         if (wzorzec[i] == '?') {
