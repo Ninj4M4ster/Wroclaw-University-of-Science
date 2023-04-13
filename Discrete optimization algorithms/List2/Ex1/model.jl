c704ac6d4ae69664b44219c9c40f7e5bc28e9740
Ninj4M4ster <kuba.drzewiecki@icloud.com>
2023-04-14 01:34:43 +0200

    Code refactor, added missing comments

--- Discrete optimization algorithms/List2/Ex1/model.jl
+++ Discrete optimization algorithms/List2/Ex1/model.jl
@@ -1,22 +1,43 @@
 import Pkg
 Pkg.add("JuMP")
 import Pkg
-Pkg.add("Clp")
+Pkg.add("HiGHS")
 
 using JuMP
-using Clp
+using HiGHS
 
-model = Model(Clp.Optimizer)
+model = Model(HiGHS.Optimizer)
 
-@variable(model, 0 <= x1 <= 275000)
-@variable(model, 0 <= x2 <= 550000)
-@variable(model, 0 <= x3 <= 660000)
+# Koszty paliw wybranej firmy na kazdym z lotnisk
+costs = [
+    10 7 8
+    10 11 14
+    9 12 4
+    11 13 9
+]
 
-@variable(model, y1 >= 110000)
-@variable(model, y2 >= 220000)
-@variable(model, y3 >= 330000)
-@variable(model, y4 >= 440000)
+n = size(costs)[1]
+m = size(costs)[2]
 
-@objective(model, Min, 40*x1 + 43*x2 + 35*x3)
+# Zapotrzebowanie na paliwo na poszczegolnych lotniskach
+needed_fuel_ammount = [110000 220000 330000 440000]
 
-@constraint(model, c1, y1 + y2 + y3 + y4 == x1 + x2 + x3)
+# Paliwo dostepne u kazdego z dostawcow
+available_fuel = [275000 550000 660000]
+
+# Macierz zmiennych decyzyjnych
+@variable(model, X[1:n, 1:m] >= 0)
+
+# Ograniczenia na dostepne paliwo
+@constraint(model, sum(X[:, 1]) <= available_fuel[1])
+@constraint(model, sum(X[:, 2]) <= available_fuel[2])
+@constraint(model, sum(X[:, 3]) <= available_fuel[3])
+
+# Ograniczenia na zapotrzebowanie na paliwo na poszczegolnych lotniskach
+@constraint(model, needed_fuel_ammount[1] == sum(X[1, :]))
+@constraint(model, needed_fuel_ammount[2] == sum(X[2, :]))
+@constraint(model, needed_fuel_ammount[3] == sum(X[3, :]))
+@constraint(model, needed_fuel_ammount[4] == sum(X[4, :]))
+
+# Funkcja celu
+@objective(model, Min, sum(X .* costs))
--- Discrete optimization algorithms/List2/Ex3/model.jl
+++ Discrete optimization algorithms/List2/Ex3/model.jl
@@ -9,20 +9,24 @@ using HiGHS
 
 model = Model(HiGHS.Optimizer)
 
+# Minimalna ilosc dostepnych radiowozow w dzielnicach podczas poszczegolnych zmiana
 G_min = [
     2 4 3
     3 6 5
     5 7 6
 ]
 
+# Maksymalna ilosc dostepnych radiowozow w dzielnicach podczas poszczegolnych zmian
 G_max = [
     3 7 5
     5 7 10
     8 12 10
 ]
 
+# Minimalna sumaryczna ilosc radiowozow dla poszczegolnych zmian
 shift_min = [10, 20, 18]
 
+# Minimalna sumaryczna ilosc radiowozow dla poszczegolnych dzielnic
 district_min = [10, 14, 13]
 
 n = size(G_min)[1]
@@ -30,10 +34,14 @@ n = size(G_min)[1]
 # Zmienna decyzyjna
 @variable(model, result_graph[1:n, 1:n] >= 0)
 
+# Zmienna decyzyjna musi sie zawierac w przedziale wyznaczonym przez minimalne i maksymalne ilosci dostepnych radiowozow
 @constraint(model, [i = 1:n, j = 1:n], G_min[i, j] <= result_graph[i, j] <= G_max[i, j])
 
+# Ograniczenie na minimalna sumaryczna ilosc radiowozow w poszczegolnych dzielnicach
 @constraint(model, [i = 1:n], sum(result_graph[i, :]) >= district_min[i])
 
+# Ograniczenie na minimalna sumaryczna ilosc radiowozow podczas poszczegolnych zmian
 @constraint(model, [i = 1:n], sum(result_graph[:, i]) >= shift_min[i])
 
+# Funkcja celu
 @objective(model, Min, sum(result_graph[:, :]))
