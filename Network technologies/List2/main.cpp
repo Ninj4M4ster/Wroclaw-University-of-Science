0f2d851b6138f84c552b72d816aca7579c605f17
Ninj4M4ster <kuba.drzewiecki@icloud.com>
2023-04-20 14:37:34 +0200

    Added more graphs

--- Network technologies/List2/main.cpp
+++ Network technologies/List2/main.cpp
@@ -1,10 +1,10 @@
 #include "network.cpp"
 #include <memory>
-#define N 1000000
+#define N 10000
 
 int main() {
-  std::string graph_file = "Network technologies/List2/graph.txt";
-  std::string intensity_matrix_file = "Network technologies/List2/intensity_matrix.txt";
+  std::string graph_file = "Network technologies/List2/graphs/graph.txt";
+  std::string intensity_matrix_file = "Network technologies/List2/intensity_matrices/intensity_matrix.txt";
   std::shared_ptr<network::Network> net =
       std::make_shared<network::Network>(graph_file,
                                          intensity_matrix_file,
