a4bf5aad9939133c85c6e13f3f2b206d5bdd315d
Ninj4M4ster <kuba.drzewiecki@icloud.com>
2023-05-12 18:36:51 +0200

    Added statistics after model training, changed model. Added my test data

--- AI/List3/Ex1/model.py
+++ AI/List3/Ex1/model.py
@@ -3,12 +3,13 @@ import numpy as np
 from tensorflow import keras
 import os
 import matplotlib.pyplot as plt
+from PIL import Image
 
 
 def create_model():
     model = keras.models.Sequential([
         keras.layers.Flatten(input_shape=(28, 28)),
-        keras.layers.Dense(128, activation="relu", kernel_regularizer=keras.regularizers.l2(0.0001)),
+        keras.layers.Dense(128, activation="relu"),
         keras.layers.Dropout(0.5),
         keras.layers.Dense(10)
     ])
@@ -27,11 +28,33 @@ def train_model(model, train_images, train_labels, test_images, test_labels, wei
                                                save_weights_only=True,
                                                verbose=1)
 
-    model.fit(train_images,
-              train_labels,
-              epochs=10,
-              validation_data=(test_images, test_labels),
-              callbacks=[callback])
+    history = model.fit(train_images,
+                        train_labels,
+                        epochs=10,
+                        validation_data=(test_images, test_labels),
+                        callbacks=[callback])
+
+    acc = history.history['accuracy']
+    val_acc = history.history['val_accuracy']
+
+    loss = history.history['loss']
+    val_loss = history.history['val_loss']
+
+    epochs_range = range(10)
+
+    plt.figure(figsize=(8, 8))
+    plt.subplot(1, 2, 1)
+    plt.plot(epochs_range, acc, label='Training Accuracy')
+    plt.plot(epochs_range, val_acc, label='Validation Accuracy')
+    plt.legend(loc='lower right')
+    plt.title('Training and Validation Accuracy')
+
+    plt.subplot(1, 2, 2)
+    plt.plot(epochs_range, loss, label='Training Loss')
+    plt.plot(epochs_range, val_loss, label='Validation Loss')
+    plt.legend(loc='upper right')
+    plt.title('Training and Validation Loss')
+    plt.show()
 
 
 def load_weights(model, weights_file_name):
@@ -39,19 +62,44 @@ def load_weights(model, weights_file_name):
     model.load_weights(checkpoint_path)
 
 
+def create_labels():
+    return ["Zero", "Jeden", "Dwa", "Trzy", "Cztery", "Piec", "Szesc", "Siedem", "Osiem", "Dziewiec"]
+
+
 def main():
     digits = keras.datasets.mnist
     (train_digits, train_labels), (test_digits, test_labels) = digits.load_data()
     model = create_model()
-    # load_weights(model, "minst_model.ckpt")
+    load_weights(model, "minst_model.ckpt")
 
     train_digits = train_digits / 255.0
     test_digits = test_digits / 255.0
 
-    train_model(model, train_digits, train_labels, test_digits, test_labels, "minst_model.ckpt")
+    my_labels = create_labels()
 
-    loss, acc = model.evaluate(test_digits, test_labels)
-    print(f"Accuracy: {acc * 100:5.2f}")
+    # train_model(model, train_digits, train_labels, test_digits, test_labels, "minst_model.ckpt")
+
+    # loss, acc = model.evaluate(test_digits, test_labels)
+    # print(f"Accuracy: {acc * 100:5.2f}")
+
+    my_digit_name = "my_test_data/7.bmp"
+    my_digit = Image.open(my_digit_name)
+    # my_digit = my_digit.resize((28, 28))
+    my_digit = np.asarray(my_digit)
+
+    my_digit = my_digit / 255.0
+
+    # plt.figure()
+    # plt.imshow(my_digit)
+    # plt.colorbar()
+    # plt.grid(False)
+    # plt.show()
+
+    my_digit = (np.expand_dims(my_digit, 0))
+
+    prediction = model.predict(my_digit)
+    score = tf.nn.softmax(prediction[0])
+    print(my_labels[np.argmax(score)])
 
 
 if __name__ == '__main__':
