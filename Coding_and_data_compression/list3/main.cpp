7cb3901cf9c7a5edfb864a0c3a1faf23808c2e0e
Ninj4M4ster <kuba.drzewiecki@icloud.com>
2023-11-27 21:42:52 +0100

    Last minor fix

--- Coding_and_data_compression/list3/main.cpp
+++ Coding_and_data_compression/list3/main.cpp
@@ -3,12 +3,13 @@
 
 int main(int argc, char* argv[]) {
   std::cout << "Start\n";
+  CodingType coding_type_ = CodingType::OMEGA;
   Coder coder;
-  coder.setCodingType(CodingType::DELTA);
+  coder.setCodingType(coding_type_);
   coder.compress("data/pan-tadeusz-czyli-ostatni-zajazd-na-litwie.txt", "compressed.txt");
   std::cout << "Compressed\n";
   Decoder decoder;
-  decoder.setCodingType(CodingType::DELTA);
+  decoder.setCodingType(coding_type_);
   decoder.decompress("compressed.txt", "decompressed.txt");
   std::cout << "Decompressed\n";
 }
